37d10267316180c73518247fc6b2a8da80ccee8f
Cameron <cameron@harves.net>
2023-07-23 23:40:31 +0800

    yeah i know

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,10 +1,35 @@
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = `Hello World`;
-    element.classList.add('hello');
 
-    return element;
-  }
-  
-  document.body.appendChild(component());
+// Ignore the DOM Stuff to start with!
+//
+// Make the object constructors
+//
+
+
+const exampleCategory = {
+    title: "general household tasks",   // User inputted string (will need to be sanitised)
+    description: "tasks for the fam",   // User inputted string (will need to be sanitised)
+    todoContainer: []
+};
+
+const exampleTodoObject = {
+    title: "shopping",                  // User inputted string (will need to be sanitised)
+    dueDate: "someDate",                // Handle this with some date package
+    priority: "High",                   // Low - Medium - High
+    status: "Done",                     // Done - Not Done
+    // checklist: ["eggs","milk","flour"]  // Multiple user inputted strings (will need to be sanitised)
+};
+
+exampleCategory.todoContainer.push(exampleTodoObject);
+
+console.log(exampleCategory);
+
+
+// 'todo' class constructor
+class Todo {
+    constructor(title, dueDate, priority, status) {
+        this.title = title,
+        this.dueDate = dueDate,
+        this.priority = priority,
+        this.status = status
+    }
+};
