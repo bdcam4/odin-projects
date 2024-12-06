c184609785076e6ac315792decd4e45215115614
Cameron <cameron@harves.net>
2024-12-07 00:03:37 +0800

    Project: Binary Search Trees

---
+++ binary-search-tree/index.js
@@ -0,0 +1,36 @@
+import { Tree } from "./binary-search-tree.js";
+
+// const arr = [2,4,6,8,10,12];
+// const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+const arr = [1,2,3,4,5,6,7,8,9,10];
+const myTree = new Tree(arr);
+
+myTree.drawTree();
+
+// myTree.rebalance();
+// console.log(myTree.isBalanced());
+// console.log(myTree.depth(9));
+// console.log(myTree.height());
+// let myNode = myTree.root.right.right;
+// console.log(myNode);
+// console.log(myTree.height(myNode));
+// console.log(myTree.postOrder());
+// myTree.postOrder(console.log);
+// console.log(myTree.preOrder());
+// myTree.preOrder(console.log);
+// console.log(myTree.inOrder());
+// myTree.inOrder(console.log);
+// console.log(myTree.levelOrder());
+// myTree.levelOrder(console.log);
+// console.log(myTree.find(18).key);
+// myTree.deleteNode(15);
+
+// myTree.insert(20);
+// myTree.insert(30);
+// myTree.insert(100);
+
+console.log('________________');
+
+
+// myTree.drawTree();
+
